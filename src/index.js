aaa6406a9265fc0e2ad6e11cf6dc3fb7e740e033
José Carlos Alarcón Murillo <josecarlos.alar@gmail.com>
2023-01-21 12:43:01 +0100

    update loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+import logoImg from './content/logo.png';
+import "./styles.scss";
+//import "./mystyles.scss";
+
+const img = document.createElement('img');
+img.src = logoImg;
+
+document.getElementById('imgContainer').appendChild(img);
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    context: path.resolve(__dirname, "./src"),
+    entry: {
+        app: "./index.js",
+        vendorStyles: ["../node_modules/bootstrap/dist/css/bootstrap.css"],
+    },
+    output: {
+        filename: "[name].[chunkhash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                type: "asset/resource",
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+            },
+        ],
+    },
+    plugins: [
+        //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: "index.html", //Name of file in ./dist/
+            template: "./index.html", //Name of template in ./src
+            scriptLoading: "blocking", // Just use the blocking approach (no modern defer or module)
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    devServer: {
+        static: path.join(__dirname, "./src"),
+    },
+};
